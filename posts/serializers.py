f8ad6cae581c0665b46d097b6c5da7bb87e908f7
GJSayers <gemma.sayers1@btinternet.com>
2022-05-31 18:26:43 +0000

    Create Postlist view to view and create posts

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,10 +1,10 @@
 from rest_framework import serializers
-from .models import Post
+from posts.models import Post
 
 class PostSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
-    profile_id = serializers.ReadOnlyField(source='owner.id')
+    profile_id = serializers.ReadOnlyField(source='owner.profile.id')
     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
 
     def validate_image(self, value):
