a22280f80cb8130571da75bd6b28473bc6f287b0
GJSayers <gemma.sayers1@btinternet.com>
2022-06-03 17:23:23 +0000

    Create Comments app and serializers

---
+++ comments/serializers.py
@@ -0,0 +1,31 @@
+from rest_framework import serializers
+from comments.models import Comment
+
+class CommentSerializer(serializers.ModelSerializer):
+    """
+    Serializer for the Comment model, Adding 3 extra on returning list of Comments
+    """
+    owner = serializers.ReadOnlyField(source='owner.username')
+    is_owner = serializers.SerializerMethodField()
+    profile_id = serializers.ReadOnlyField(source='owner.profile.id')
+    profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
+
+    def get_is_owner(self, obj):
+        request = self.context['request']
+        return request.user == obj.owner
+
+    class Meta:
+        model = Comment
+        fields = [
+            'id', 'owner', 'post', 'created_at', 'updated_at', 'is_owner',
+            'profile_id', 'profile_image', 'content',
+        ]
+
+
+class CommentDetailSerializer(CommentSerializer):
+    """
+    Serializer for the Comment model in Detail View, post is a read only field 
+    that does not resetting on each update
+    """
+    post = serializers.ReadOnlyField(source='post.id')
+    
