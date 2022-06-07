420d4ff8da02482a99d35504afa43570134cd1a8
GJSayers <gemma.sayers1@btinternet.com>
2022-06-07 16:18:09 +0000

    Add extra fields to & refactor Post views

--- posts/views.py
+++ posts/views.py
@@ -1,100 +1,39 @@
-from django.http import Http404
-from rest_framework import status, permissions
-from rest_framework.response import Response
-from rest_framework.views import APIView
+from django.db.models import Count
+from rest_framework import generics, filters, permissions
 from .models import Post
 from .serializers import PostSerializer
 from drf_api.permissions import IsOwnerOrReadOnly
 
 
-class PostList(APIView):
+class PostList(generics.ListCreateAPIView):
     """
-    List all posts (this view can be refactored to use generics)
-    """
-    serializer_class = PostSerializer
-    permission_classes = [
-        permissions.IsAuthenticatedOrReadOnly
-    ]
-
-    def get(self, request):
-        """
-        Get all posts (this view can be refactored to use generics)
-        """
-        posts = Post.objects.all()
-        serializer = PostSerializer(
-            posts, many=True, context={'request': request}
-        )
-        return Response(serializer.data)
-
-    def post(self, request):
-        """
-        Create a post  (this view can be refactored to use generics)
-        """
-        serializer = PostSerializer(
-            data=request.data, context={'request': request}
-        )
-        if serializer.is_valid():
-            serializer.save(owner=request.user)
-            return Response(
-                serializer.data, status=status.HTTP_201_CREATED
-            )
-        return Response(
-            serializer.errors, status=status.HTTP_400_BAD_REQUEST
-        )
-
-
-class PostDetail(APIView):
-    """
-    View post detail (this view can be refactored to use generics)
+    List all posts 
     """
     serializer_class = PostSerializer
-    permission_classes = [
-        permissions.IsAuthenticatedOrReadOnly
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Post.objects.annotate(
+        comments_count=Count('comment', distinct=True),
+        likes_count=Count('likes', disticnt=True)
+    ).order_by('-created_at')
+    filter_backends = [
+        filters.OrderingFilter
     ]
+    ordering_fields = [
+        'comments_count',
+        'likes_count',
+        'likes__created_at',
+    ]
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
 
-    def get_object(self, pk):
-        """
-        Get post detail object or 404
-        """
-        try:
-            post = Post.objects.get(pk=pk)
-            self.check_object_permissions(self.request, post)
-            return post
-        except Post.DoesNotExist:
-            raise Http404
 
-    def get(self, request, pk):
-        """
-        Get post detail (this view can be refactored to use generics)
-        """
-        post = self.get_object(pk)
-        serializer = PostSerializer(
-            post,
-            context={'request': request}
-            )
-        return Response(serializer.data)
-
-    def put(self, request, pk):
-        """
-        Update post (this view can be refactored to use generics)
-        """
-        post = self.get_object(pk)
-        serializer = PostSerializer(
-            post,
-            data=request.data,
-            context={'request': request}
-            )
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, pk):
-        """
-        Update post (this view can be refactored to use generics)
-        """
-        post = self.get_object(pk)
-        post.delete()
-        return Response(
-            status=status.HTTP_204_NO_CONTENT
-        )
+class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    """
+    View post detail 
+    """
+    serializer_class = PostSerializer
+    permission_classes = [IsOwnerOrReadOnly]
+    queryset = Post.objects.annotate(
+        comments_count=Count('comment', distinct=True),
+        likes_count=Count('likes', disticnt=True)
+    ).order_by('-created_at')
