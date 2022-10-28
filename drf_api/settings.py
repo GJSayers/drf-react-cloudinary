ee0d274bd56c22f171300044363580977cd644c1
GJSayers <gemma.sayersm@gmail.com>
2022-10-28 14:39:38 +0000

    Add Render hostname to settings

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -63,6 +63,11 @@ DEBUG = 'DEV' in os.environ
 
 ALLOWED_HOSTS = ['localhost']
 
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+
+
 # Application definition
 
 INSTALLED_APPS = [
